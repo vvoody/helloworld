fad2ad213779c934fa37ff0502da1f917087c584
vvoody <ydoovv@gmail.com>
2010-04-13 15:14:20 +0800

    pickupnewords.py: Just works :D     Enable to store the known words into db

--- python/pickupnewords/pickupnewords.py
+++ python/pickupnewords/pickupnewords.py
@@ -24,6 +24,13 @@ def pickup_words(filename):
                     words.append(w.lower())
     return words
 
+help_doc = """
+0,9,13, 21
+all
+!0,9,13,21
+!all -> RET
+"""
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print "Where the file(s)?"
@@ -32,8 +39,39 @@ if __name__ == "__main__":
         wordslist = []
         for f in sys.argv[1:]:
             wordslist += pickup_words(f)
-        new_words = set(wordslist)
-        f = open('oldwords.dat', 'r')
-        old_words = pickle.load(f)
-        print new_words - old_words
-        f.close()
+        oldf = open('oldwords.dat', 'r+')
+        old_words = pickle.load(oldf)
+        oldf.seek(0)
+        new_words = list(set(wordslist) - old_words)
+        for i in range(len(new_words)):
+            print i, '"%s"' % new_words[i]
+        #sys.exit()
+        # Store the words you have already known, cmd not check carefully
+        cmd = raw_input("Stroe the words already known: ").strip(' ,')
+        if cmd [:1] == '?' or cmd[:1] == 'h' or cmd[:4] == 'help':
+            print help_doc
+        # all words are new
+        elif cmd[:4] == '!all' or cmd == '!' or cmd == '':
+            pass
+        # all words are known
+        elif cmd[:3] == 'all':
+            old_words = old_words.union(set(new_words))
+            pickle.dump(old_words, oldf)
+        # Store except
+        elif cmd[0] == '!':
+            old_words= old_words.union(
+                set(new_words) -
+                set([new_words[int(i)] for i in cmd[1:].split(',')])
+                )
+            pickle.dump(old_words, oldf)
+        # Store exactly
+        elif cmd[0] in '1234567890':
+            old_words= old_words.union(
+                set([new_words[int(i)] for i in cmd[1:].split(',')])
+                )
+            pickle.dump(old_words, oldf)
+        else:
+            print "Unknown command!"
+        #
+        oldf.close()
+# EOF
