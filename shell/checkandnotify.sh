dd14aae39e89d2757b7e2111fe4a229aadfe7fe7
vvoody <ydoovv@gmail.com>
2010-08-13 00:22:54 +0800

    Just check the master branch.

--- shell/checkandnotify.sh
+++ shell/checkandnotify.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
 cd /home/vvoody/lab/backend-2.6.1_git/
-git svn rebase | grep 'Current branch master is up to date.'
+current_branch=$(git branch | grep '*' | cut -d ' ' -f2)
+if [ current_branch != "master" ]; then
+	git checkout master
+fi
+
+result=$(git svn rebase)
+echo $result | grep 'Current branch master is up to date'
+
 if [ $? -eq 0 ]; then
 	echo $(date) ' - NO UPDATES' >> /tmp/vvchecksvnup
 else
     msg="We got svn update!"
     [ -x $(which kdialog) ] && kdialog --title "Check svn update" --passivepopup "$msg" 60
 	echo $(date) ' - GOT UPDATES' >> /tmp/vvchecksvnup
+	echo "$result" >> /tmp/vvchecksvnup
 fi
+
+git checkout $current_branch
